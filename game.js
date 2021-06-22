25b8e559c79afb94073e5c151c5ab74c2d466079
CrazySB <47658884+SmitBhamwala@users.noreply.github.com>
2021-06-22 12:40:26 +0530

    Update game.js

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ var level = 0;
 
 
 
-$("#level-title").on("keydown click tap touchstart", function (event) {
+$(document).on("keydown", function (event) {
 
     if (!started) {
         $("#level-title").html("Level " + level);
